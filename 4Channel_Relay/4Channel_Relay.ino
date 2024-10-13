c9bdc10f29c24b0b35663a25f4a26515f5c98cb8
Ayaan Waqar <72092367+AyaanWaqar@users.noreply.github.com>
2024-10-13 14:57:44 +0500

    Add files via upload

---
+++ 4Channel_Relay/4Channel_Relay.ino
@@ -0,0 +1,20 @@
+int a = 3;
+int b = 4;
+int c = 5;
+int d = 6;
+
+void setup(){
+
+pinMode(a, OUTPUT);
+pinMode(b, OUTPUT);
+pinMode(c, OUTPUT);
+pinMode(d, OUTPUT);
+
+}
+
+void loop(){
+digitalWrite(a, LOW);
+digitalWrite(b, LOW);
+digitalWrite(c, LOW);
+digitalWrite(d, LOW);
+}
